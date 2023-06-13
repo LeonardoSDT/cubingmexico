b56aec4bfbf7339108e90112d84e25530c26edfb
Leonardo Sánchez Del Toro <leotorokr@gmail.com>
2023-06-13 16:48:10 -0700

    Preparing the database cron job

--- sync_wca_database.sh
+++ sync_wca_database.sh
@@ -1,10 +1,12 @@
+#!/usr/bin/env sh
+
 dir=$(dirname "$0")
 web_dir="$dir/code"
 data_dir="$web_dir/data"
 dump_dir="$data_dir/extracted"
 
 echo "Downloading WCA Database"
-curl https://www.worldcubeassociation.org/results/misc/WCA_export.tsv.zip -o "$data_dir/WCA_export.tsv.zip"
+curl https://www.worldcubeassociation.org/export/results/WCA_export.tsv.zip -o "$data_dir/WCA_export.tsv.zip"
 
 if [ -e "$dump_dir/metadata.json" ]
 then
