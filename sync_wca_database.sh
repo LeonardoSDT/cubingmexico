c407af1780e96c73b735928b6b72c8a023e96370
Leonardo Sánchez Del Toro <leotorokr@gmail.com>
2024-02-12 13:52:18 -0700

    fix: add -L flag to the WCA database export curl

--- sync_wca_database.sh
+++ sync_wca_database.sh
@@ -4,7 +4,7 @@ dir=$(dirname "$0")
 web_dir="$dir/code"
 
 echo "Downloading WCA Database"
-curl https://www.worldcubeassociation.org/export/results/WCA_export.tsv.zip -o "/tmp/WCA_export.tsv.zip"
+curl -L https://www.worldcubeassociation.org/export/results/WCA_export.tsv.zip -o "/tmp/WCA_export.tsv.zip"
 
 echo "Unzipping archive"
 unzip -o "/tmp/WCA_export.tsv.zip" -d "/tmp"
